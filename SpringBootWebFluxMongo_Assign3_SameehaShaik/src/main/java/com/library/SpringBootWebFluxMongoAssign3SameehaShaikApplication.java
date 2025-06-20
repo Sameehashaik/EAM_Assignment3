bd699fe70dc35c1dcf575b68ce3469c35f97447c
Sameeha Shaik <sameeha1100@gmail.com>
2025-06-20 18:15:55 -0400

    added code

---
+++ SpringBootWebFluxMongo_Assign3_SameehaShaik/src/main/java/com/library/SpringBootWebFluxMongoAssign3SameehaShaikApplication.java
@@ -0,0 +1,13 @@
+package com.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebFluxMongoAssign3SameehaShaikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebFluxMongoAssign3SameehaShaikApplication.class, args);
+	}
+
+}
